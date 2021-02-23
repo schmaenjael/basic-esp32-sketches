8b399dc2dcef9d8c93326e34f35bab6b54d7603f
Schmaenjael <63205793+schmaenjael@users.noreply.github.com>
2021-02-23 21:48:36 +0100

    added additional comments to KeyClick Sketch

--- Sketch_KeyClick/Sketch_KeyClick.ino
+++ Sketch_KeyClick/Sketch_KeyClick.ino
@@ -6,21 +6,21 @@
  * Refernce: https://github.com/espressif/arduino-esp32/
  */
 
-const byte red_LED = 33;
-const byte blue_LED = 27;
-const byte green_LED = 15;
-const byte VOUT_PIN = 32;
-const int FREQ = 5000;
-int led_CHANNEL = 0;
-int RES = 8;
+const byte red_LED = 33;   // initializing the red LED
+const byte blue_LED = 27;  // initializing the blue LED
+const byte green_LED = 15; // initializing the green LED
+const byte VOUT_PIN = 32;  // initializing the VOUT Pin
+const int FREQ = 5000;     // setting the frequence to 5000
+int led_CHANNEL = 0;       // setting the LED_Channel to 0
+int RES = 8;               // setting the resolution to 8
 
-const byte m_T1 = 213;
-const byte m_T2 = 170;
-const byte m_T3 = 128;
-const byte m_T4 = 85;
-const byte m_T5 = 39;
-const byte m_T6 = 1;
-int m = 0;
+const byte m_T1 = 213; // sets the key T1 to a specific value
+const byte m_T2 = 170; // sets the key T2 to a specific value
+const byte m_T3 = 128; // sets the key T3 to a specific value
+const byte m_T4 = 85;  // sets the key T4 to a specific value
+const byte m_T5 = 39;  // sets the key T5 to a specific value
+const byte m_T6 = 1;   // sets the key T6 to a specific value
+int m = 0;             // sets the value of m to 0
 
 void setup()
 {
